c784bb9e34ada68e24b91acbb9fafa0cfd730565
david-dlsa <david.andrade.dev@gmail.com>
2024-08-01 16:58:07 -0300

    Return to original screen
    acesso por botao 1 e 3 pra scenes diferentes

--- Build/Muro_4_5_1_WebPage.framework.js
+++ Build/Muro_4_5_1_WebPage.framework.js
@@ -1994,13 +1994,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  5978208: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 5978269: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 5978333: function() {return Module.webglContextAttributes.powerPreference;},  
- 5978391: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 5978446: function($0) {performance.now = function() { return $0; };},  
- 5978494: function($0) {performance.now = function() { return $0; };},  
- 5978542: function() {performance.now = Module['emscripten_get_now_backup'];}
+  5978240: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 5978301: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 5978365: function() {return Module.webglContextAttributes.powerPreference;},  
+ 5978423: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 5978478: function($0) {performance.now = function() { return $0; };},  
+ 5978526: function($0) {performance.now = function() { return $0; };},  
+ 5978574: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
